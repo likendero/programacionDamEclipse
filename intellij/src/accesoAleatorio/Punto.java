8c91ed0c99b5650a15c7d647e6d54c59fa972ff1
Unknown <likendero11@gmail.com>
2018-04-14 19:54:08 +0200

    coss del ultimo dia, editorial

---
+++ intellij/src/accesoAleatorio/Punto.java
@@ -0,0 +1,17 @@
+package accesoAleatorio;
+
+/**
+ * clase con todos sus metodos
+ * metodo distancia()
+ * metodo puntoMedio()
+ * --------------
+ * annadir valores
+ * crear fichero
+ * annadir punto
+ * cambiar punto
+ * distancia entre dos puntos
+ * punto medio entre dos puntos
+ */
+public class Punto {
+
+}
